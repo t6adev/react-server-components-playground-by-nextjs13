58c2be2a81bd9d8d597991c7c09efabc0ee9b036
t6adev <t.yamamoto28@gmail.com>
2023-05-06 15:27:19 +0900

    chore

--- app/combo/ClientA.tsx
+++ app/combo/ClientA.tsx
@@ -1,16 +1,12 @@
-"use client";
-import { ReactNode } from "react";
-import { useAutoCountUp } from "./useAutoCountUp";
+'use client';
+import { ReactNode } from 'react';
+import { useAutoCountUp } from './useAutoCountUp';
 
-export default function ClientA({
-  anyServerComponent,
-}: {
-  anyServerComponent: ReactNode;
-}) {
+export default function ClientA({ anyServerComponent }: { anyServerComponent: ReactNode }) {
   const count = useAutoCountUp();
-
+  console.log('in ClientA');
   return (
-    <div style={{ border: "solid 1px blue", padding: 8 }}>
+    <div style={{ border: 'solid 1px blue', padding: 8 }}>
       <div>This is a client component. {count}</div>
       {anyServerComponent}
     </div>
